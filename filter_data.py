37900194d71fd806a2a924c9cc90c4ce37d7811d
raptordoug <raptordoug@gmail.com>
2016-10-13 21:43:43 -0400

    take input from a directory and a list of files

--- filter_data.py
+++ filter_data.py
@@ -6,18 +6,29 @@ import xmltodict
 import lxml.objectify
 import argparse
 import re
+import os
+import fnmatch
 
 
 parser = argparse.ArgumentParser(prog='PROG', usage='%(prog)s [options]')
-parser.add_argument('-s', action='store', dest='startpattern',help='Start of the match pattern')
-parser.add_argument('-e', action='store', dest='endpattern',help='End of the match pattern')
-parser.add_argument('-i', action='store', dest='ignorepattern',help='ignore lines with this')
+parser.add_argument('-s', action='store', dest='startpattern',help='Start of the match pattern',default='[a-zA-Z0-9]')
+parser.add_argument('-e', action='store', dest='endpattern',help='End of the match pattern',default='^#StopAndIgnore')
+parser.add_argument('-i', action='store', dest='ignorepattern',help='ignore lines with this',default='^#IgnoreThis')
+parser.add_argument('-d', action='store', dest='indir',help='ignore lines with this',default='.\\')
 parser.add_argument('-f', action='store', dest='infile',help='Input file name')
-args = parser.parse_args()
+#args = parser.parse_args()
+args, unknown = parser.parse_known_args()
 
 
-infile = (args.infile)
-#startpat = (sys.argv[2])
+infile = args.infile
+indir = args.indir
+sourcefiles = fnmatch.filter(os.listdir(indir),infile)
+#print sourcefiles
+print "start pattern: " + args.startpattern
+print "end pattern: " + args.endpattern
+print "ignore pattern: " + args.ignorepattern
+print "file name: " + args.infile
+
 startpattern = re.compile(args.startpattern)
 endpattern = re.compile(args.endpattern)
 ignorepattern = re.compile(args.ignorepattern)
@@ -25,20 +36,21 @@ ignorepattern = re.compile(args.ignorepattern)
 printdata = False
 ignoreline = False
 
-
-for line in fileinput.input(infile):
-    if startpattern.search(line):
-        printdata=True
-    if ignorepattern.search(line):
-        printdata=False
-        ignoreline = True
+for filelist in sourcefiles:
+    #print "opening file named" + filelist
+    for line in fileinput.input(indir + filelist):
+        if startpattern.search(line):
+            printdata=True
+        if ignorepattern.search(line):
+            printdata=False
+            ignoreline = True
     #only print stuff in the middle
-    if printdata and len(line) >0:
-        sys.stdout.write(line)
+        if printdata and len(line) >0:
+            sys.stdout.write(line)
     #reset after the printline
-    if ignoreline:
-        ignoreline = False
-    if endpattern.search(line):
-        printdata = False
+        if ignoreline:
+            ignoreline = False
+        if endpattern.search(line):
+            printdata = False
 
 
